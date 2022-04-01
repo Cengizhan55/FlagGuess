65233f0e684cbfa5c784e6c3f3a971065f26f738
Cengizhan55 <cengiz_er_@hotmail.com>
2022-04-01 17:55:40 +0300

    Initial commit

---
+++ FlagGuess/ContentView.swift
@@ -0,0 +1,125 @@
+//
+//  ContentView.swift
+//  FlagGuess
+//
+//  Created by Cengizhan Er on 1.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var yourScore = 0
+    @State private var degrees = 0
+    @State private var availableNumber = 0
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            if(flagTapped(number)){
+                                withAnimation(.easeIn(duration: 1)){
+                                    degrees += 360
+                                }
+                            }
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                                .rotation3DEffect(
+                                    .degrees(
+                                        (number == availableNumber) ?
+                                        Double(degrees) : 0.0),
+                                    axis: (x : 0 ,y: 1, z : 0))
+                            
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(yourScore)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(yourScore)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) -> Bool {
+        
+        if number == correctAnswer {
+            availableNumber = number
+            scoreTitle = "Correct"
+            yourScore += 1
+            showingScore = true
+            return true
+            
+        } else {
+            
+            scoreTitle = "Wrong"
+            if (yourScore == 0){
+                print("you lose ")
+            }else{
+                yourScore -= 1
+            }
+            
+        }
+        
+        showingScore = true
+        return false
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagGuess/FlagGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessApp.swift
+//  FlagGuess
+//
+//  Created by Cengizhan Er on 1.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
